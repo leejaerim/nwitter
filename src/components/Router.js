86970c280ea5a8e14430160bec24a069053b0032
leejaerim <ger6608@naver.com>
2022-09-30 22:31:00 +0900

    End of First Recture

---
+++ src/components/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+import { BrowserRouter, Routes, HashRouter as Router, Route } from "react-router-dom";
+import Auth from '../routes/Auth'
+import Home from '../routes/Home'
+const AppRouter =() => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // <> -> fragment 많은 요소를 랜더링 하고 싶을 때 사용합니다.
+    return (
+        <BrowserRouter>
+            <Routes>
+            {isLoggedIn ? (<><Route exact path="/" element={<Home></Home>}></Route></>) :(<><Route exact path="/" element= {<Auth></Auth>}></Route></>)}
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+
+export default AppRouter
